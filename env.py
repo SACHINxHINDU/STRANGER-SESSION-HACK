83728a63d2f8a0ad49001ca09e50fa6d75455f93
SACHINxHINDU <sachinxxsanatani@gmail.com>
2024-07-30 21:42:17 +0530

    Update env.py

--- env.py
+++ env.py
@@ -5,11 +5,11 @@ load_dotenv()
 
 API_ID = os.getenv("API_ID", "27383453")
 API_HASH = os.getenv("API_HASH", "4c246fb0c649477cc2e79b6a178ddfaa")
-BOT_TOKEN = os.getenv("BOT_TOKEN", None)
-SUDOERS = list(map(int, os.getenv("SUDOERS", "6762113050").split()))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7439140731:AAHk4e5ZmzdPiRt3LJ5p7rUt1nD2xwbF2UQ")
+SUDOERS = list(map(int, os.getenv("SUDOERS", "5959548791").split()))
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
-LOG_GROUP_ID = os.getenv("LOG_GROUP_ID", "-1002018556839")
-MUST_JOIN = os.getenv("MUST_JOIN", "SHIVANSH474")
+LOG_GROUP_ID = os.getenv("LOG_GROUP_ID", "-1001970127211")
+MUST_JOIN = os.getenv("MUST_JOIN", "all_sanatani_bot")
 DISABLED = os.getenv("DISABLED", "").split()
 
 if not API_ID:
